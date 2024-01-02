90f4b9703f764e24e73008627402ce3d6b55500b
sunghyun1014 <sunghyun1014@naver.com>
2024-01-03 01:35:09 +0900

    css 수정

--- src/main/reactfront/src/components/Navi.js
+++ src/main/reactfront/src/components/Navi.js
@@ -18,39 +18,41 @@ const Span = styled.span`
 function Navi() {
   const [modalOpen, setModalOpen] = useState(false);
   const modalBackground = useRef();
-  
+
   const onClicks = () => {
-      //navigate("/trip");
-      setModalOpen(true);
-    };
+    //navigate("/trip");
+    setModalOpen(true);
+  };
 
   return (
     <>
-    <div className="nav_container">
-      <div>
-        <div className="logo"></div>
+      <div className="nav_container">
+        <div>
+          <div className="logo"></div>
 
-        <div className="nav">
-          <StyleLink to="/notfound">여행지</StyleLink>
-          <StyleLink to="/notfound">고객지원</StyleLink>
-          <StyleLink to="/notfound">이용방법</StyleLink>
-          <Span onClick={onClicks}>로그인</Span>
-        </div>
-      </div>
-    </div>
-    {
-        modalOpen ?
-        <div className={'modal_container'} ref={modalBackground} onClick={e => {
-          if (e.target === modalBackground.current) {
-            setModalOpen(false);
-          }
-        }}>
-          <div className={'modal_box'}>
-          <LoginForm closeModal={() => setModalOpen(false)}></LoginForm>
+          <div className="nav">
+            <StyleLink to="/notfound">여행지</StyleLink>
+            <StyleLink to="/notfound">고객지원</StyleLink>
+            <StyleLink to="/notfound">이용방법</StyleLink>
+            <Span onClick={onClicks}>로그인</Span>
           </div>
         </div>
-        : null
-      }
+      </div>
+      {modalOpen ? (
+        <div
+          className={"modal_container"}
+          ref={modalBackground}
+          onClick={(e) => {
+            if (e.target === modalBackground.current) {
+              setModalOpen(false);
+            }
+          }}
+        >
+          <div className={"modal_box"}>
+            <LoginForm closeModal={() => setModalOpen(false)}></LoginForm>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 }
