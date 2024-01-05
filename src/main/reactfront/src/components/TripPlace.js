07d55af724e7474ae50b485a64ac2e4831edd6a7
lokyyyi <jyw9011@naver.com>
2024-01-05 16:35:11 +0900

    commti test

--- src/main/reactfront/src/components/TripPlace.js
+++ src/main/reactfront/src/components/TripPlace.js
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
 import { TripWrapper, Tripbox } from "./TripComponents";
 import styled from "styled-components";
-import { AnimatePresence, motion } from "framer-motion";
 import axios from "axios";
-// const TripSelect = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   width: 30px;
-//   background-color: antiquewhite;
+import { auth } from "../firebase";
 
-//   &.on {
-//     min-width: 300px;
-//   }
-// `;
+const TripSelect = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30px;
+  background-color: antiquewhite;
 
-const PlaceWrapper = styled(TripWrapper)`
-  padding-top: 2rem;
+  &.on {
+    min-width: 300px;
+  }
 `;
 
+const PlaceWrapper = styled(TripWrapper)`
+  padding-top: 40px;
+`;
+const ModeController = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 50%;
+  z-index: 1;
+  transform: translateY(-50%);
+  right: -45px;
+  width: 40px;
+  height: 50px;
+  padding: 25px;
+  background-color: white;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+`;
 const SelectAreaUl = styled.ul`
-  width: 375px;
   display: grid;
   list-style: none;
   grid-template-columns: repeat(4, 90px);
@@ -35,35 +50,18 @@ const Li = styled.li`
   background-color: #cbf0f4;
   cursor: pointer;
 `;
-const TripArea = styled(motion.div)`
-  width: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const Motion_AreaName = styled(motion.div)`
-  position: relative;
 
-  width: 375px;
-  height: 100px;
-  display: flex;
-  box-shadow: rgba(0, 0, 0, 0.04) 0px 3px 5px;
-  border-radius: 5px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-`;
-function TripPlace({ weather, dateinfo }) {
-  const [tripAreaName, setTripAreaName] = useState({
-    mainAreaName: "",
-    subAreaName: "",
-  });
+function TripPlace({ weather, tripdate }) {
+  const [tripdate1, setTripdate1] = useState({ ...tripdate });
+  const [slidemode, setSlidemode] = useState(false); // 서브창 확장, 축소
   const [locations, setLocations] = useState([]); //장소 검색 결과
-  const [mainAreaCode, setMainAreaCode] = useState(""); // 메인지역 코드
-  const [subAreaCode, setSubAreaCode] = useState("");
+  const [selectedAreaCode, setSelectedAreaCode] = useState(""); // 메인지역 코드
   const [subArea, setSubArea] = useState(); // 서브 지역 코드
   const [baseurl, setBaseurl] = useState(""); // api 호출 url
   const [params, setParams] = useState({}); // api 호출 쿼리문
-  const [citycode, setCitycode] = useState({ citycode: 1, subCitycode: 2 });
-  const Area = [
+  const [citycode, setCitycode] = useState({citycode: 1, subCitycode: 2}); //활동추천api전용 시군구 코드
+
+  const cities = [
     {
       value: 1,
       name: "서울",
@@ -134,9 +132,6 @@ function TripPlace({ weather, dateinfo }) {
     },
   ];
 
-  // const queryString = new URLSearchParams(params).toString();
-  // const requrl = `${baseurl}?${queryString}`;
-
   useEffect(() => {
     if (baseurl && Object.keys(params).length > 0) {
       const queryString = new URLSearchParams(params).toString();
@@ -152,9 +147,7 @@ function TripPlace({ weather, dateinfo }) {
           return response.json();
         })
         .then((data) => {
-          console.log(
-            `citycode:  +${citycode.citycode} + subcitycode:  +${citycode.subCitycode}`
-          );
+          console.log("citycode: " +`${citycode.citycode}` + "subcitycode: " +`${citycode.subCitycode}`)
           console.log(data);
           //console.log(Object.keys(params)[8]);
           if (Object.keys(params)[8] === "eventStartDate") {
@@ -169,10 +162,10 @@ function TripPlace({ weather, dateinfo }) {
             }
             setLocations(newLocations);
             console.log(newLocations);
-          } else if (Object.keys(params)[7] === "contentTypeId") {
+          } else if(Object.keys(params)[7] === "contentTypeId"){
             console.log("관광지");
             console.log(data);
-          } else if (Object.keys(params)[5] === "_type") {
+          } else if(Object.keys(params)[5] === "_type"){
             console.log("축제");
             console.log(data);
           } else {
@@ -194,7 +187,7 @@ function TripPlace({ weather, dateinfo }) {
 
   const searchPlace = () => {
     setLocations([]);
-    const areaCode = mainAreaCode;
+    const areaCode = selectedAreaCode;
     console.log(areaCode);
     setBaseurl("http://apis.data.go.kr/B551011/KorService1/searchFestival1");
     setParams({
@@ -213,16 +206,8 @@ function TripPlace({ weather, dateinfo }) {
 
   const SelectAreaCode = (event) => {
     //메인 지역 선택
-    setCitycode({
-      citycode: event.target.value,
-      ...citycode,
-    });
-    setTripAreaName({
-      ...tripAreaName,
-      mainAreaName: event.target.textContent,
-    });
-
-    setMainAreaCode(event.target.value);
+    setCitycode({citycode: event.target.value, subCitycode: citycode.subCitycode});
+    setSelectedAreaCode(event.target.value);
     setBaseurl("http://apis.data.go.kr/B551011/KorService1/areaCode1"); //서브지역 코드받기
     setParams({
       serviceKey:
@@ -235,26 +220,8 @@ function TripPlace({ weather, dateinfo }) {
       _type: "json",
     });
   };
-  const SelectSubAreaCode = (event) => {
-    console.log(event.target.value);
-    setCitycode({
-      subCitycode: event.target.value,
-      ...citycode,
-    });
-    setSubAreaCode(event.target.textContent);
-    setTripAreaName({
-      ...tripAreaName,
-      subAreaName: event.target.textContent,
-    });
-  };
-  const AreaVariants = {
-    hidden: { opacity: 0, scale: 0.5 },
-    visable: { opacity: 1, scale: 1 },
-    exit: { opacity: 0, scale: 0.5 },
-  };
 
-  const SelectTourList = () => {
-    //관광지 조회
+  const SelectTourList = ()=>{        //관광지 조회
     console.log(citycode);
     setBaseurl("http://apis.data.go.kr/B551011/KorService1/areaBasedList1");
     setParams({
@@ -264,15 +231,14 @@ function TripPlace({ weather, dateinfo }) {
       pageNo: "1",
       MobileOS: "ETC",
       MobileApp: "APPTest",
-      areaCode: citycode.citycode,
-      sigunguCode: citycode.subCitycode,
+      areaCode: `${citycode.citycode}`,
+      sigunguCode: `${citycode.subCitycode}`,
       contentTypeId: "12",
       _type: "json",
     });
   };
 
-  const SelectFestivalList = () => {
-    //행사,축제 조회
+  const SelectFestivalList = () =>{   //행사,축제 조회
     setBaseurl("http://apis.data.go.kr/B551011/KorService1/searchFestival1");
     setParams({
       serviceKey:
@@ -282,117 +248,90 @@ function TripPlace({ weather, dateinfo }) {
       MobileOS: "ETC",
       MobileApp: "APPTest",
       _type: "json",
-      eventStartDate: dateinfo.startDay,
-      areaCode: citycode.citycode,
+      eventStartDate: `${tripdate1.start.startDate}`,
+      areaCode: `${citycode.citycode}`,
     });
-  };
+  }
 
-  const update = () => {
-    axios
-      .get("/test/send")
-      .then((response) => {
-        alert(response.data);
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error("에러", error);
-      });
+ 
+  const update = () =>{
+    axios.get('/test/send')
+    .then(response =>{
+      alert(response.data);
+      console.log(response);
+    })
+    .catch(error =>{
+      console.error('에러', error);
+    });
   };
 
   //const sampledata = {areacode: 1};
 
-  const codeOut = () => {
-    axios
-      .post("/test/codeout", {
-        areacode: 1,
-      })
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("에러떴어요 시발", error);
-      });
+  const codeOut = () =>{
+    axios.post('/test/codeout', {
+      areacode: 1
+    })
+    .then(response =>{
+      console.log(response.data);
+    })
+    .catch(error =>{
+      console.error("에러떴어요 시발", error);
+    });
+  }
+
+  const loadingAuth = () =>{
+    console.log("사용자");
+    console.log(auth.currentUser);
+  }
+
+  const handleSlidemode = () => {
+    setSlidemode((mode) => !mode);
   };
+  const SelectSubAreaCode = () => {};
+
+
+  
+
   return (
     <PlaceWrapper>
-      <Tripbox>
+      <Tripbox style={{ width: "400px" }}>
+        <p>
+          현재 지역 날씨는? {weather.name} , {weather.temp}도 입니다
+        </p>
+
+        {tripdate1 && tripdate1.start ? (
+          <p>
+            {typeof tripdate.start.startDate === "undefined"
+              ? null
+              : `${tripdate.start.startDate}
+            (${tripdate.start.startDayOfWeek})~ ${tripdate.end.endDate}
+            (${tripdate.end.endDayOfWeek})`}
+          </p>
+        ) : (
+          <p>날짜를 선택해주세요</p>
+        )}
+
+        <button>
+          <img
+            src={"/img/fairy2.svg"}
+            alt=""
+            style={{ width: "15px", height: "15px" }}
+          />
+        </button>
+
+        <button onClick={searchPlace}>Search</button>
         {subArea ? (
           <button
             onClick={() => {
-              setMainAreaCode();
               setSubArea();
-              setTripAreaName({ mainAreaName: "", subAreaName: "" });
             }}
           >
             뒤로가기
           </button>
         ) : null}
-        <AnimatePresence>
-          <Motion_AreaName style={{ marginTop: "5px" }}>
-            {tripAreaName.mainAreaName !== "" ? (
-              <TripArea
-                key="main"
-                initial="hidden"
-                animate="visable"
-                exit="exit"
-                variants={AreaVariants}
-              >
-                <p>
-                  {tripAreaName.mainAreaName ? tripAreaName.mainAreaName : null}
-                </p>
-              </TripArea>
-            ) : null}
-            {tripAreaName.mainAreaName !== "" ? (
-              <>
-                {/* <motion.div
-                  initial={{ opacity: "0", scale: 0.5 }}
-                  animate={{ opacity: "1", scale: 1 }}
-                  exit={{ opacity: "0", scale: 0.5 }}
-                  style={{
-                    width: "2px",
-                    height: "50%",
-                    position: "absolute",
-                    top: "25%",
-                    right: "50%",
-                    zIndex: "1",
-
-                    backgroundColor: "rgba(0, 0,0, 0.1)",
-                  }}
-                ></motion.div> */}
-                <motion.img
-                  initial={{ opacity: "1", scale: 0.5 }}
-                  animate={{ opacity: "1", scale: 1 }}
-                  exit={{ opacity: "0", scale: 0.5 }}
-                  src="/img/Right.svg"
-                  style={{
-                    position: "absolute",
-                    top: "30%",
-                    right: "44%",
-                    zIndex: "1",
-                    width: "40px",
-                    Color: "gray",
-                  }}
-                ></motion.img>
-              </>
-            ) : null}
-            {tripAreaName.subAreaName !== "" ? (
-              <TripArea
-                key="sub"
-                initial="hidden"
-                animate="visable"
-                variants={AreaVariants}
-                exit="exit"
-              >
-                <p>
-                  {tripAreaName.subAreaName ? tripAreaName.subAreaName : null}
-                </p>
-              </TripArea>
-            ) : null}
-          </Motion_AreaName>
-        </AnimatePresence>
         <SelectAreaUl>
           {!subArea
-            ? Area.map((city) => (
+            ? cities.map((city) => (
                 <Li
                   key={city.value}
                   value={city.value}
@@ -405,17 +344,50 @@ function TripPlace({ weather, dateinfo }) {
                 <Li
                   key={subarea.value + Math.floor(Math.random() * 1000)}
                   value={subarea.value}
-                  onClick={SelectSubAreaCode}
+                  onClick={()=>{setCitycode({subCitycode: subarea.value, citycode:citycode.citycode})
+                                console.log({tripdate1})}}
                 >
                   {subarea.subAreaname}
                 </Li>
               ))}
+              <button onClick={update}>api test</button>
+              <button onClick={codeOut}>222 test</button>
+              <button onClick={SelectTourList}>tourList</button>
+              <button onClick={SelectFestivalList}>festivalList</button>
+              <button onClick={loadingAuth}>loadingAuth</button>
         </SelectAreaUl>
-        <button onClick={searchPlace} style={{ marginTop: "20px" }}>
-          Search
-        </button>
-        <button onClick={SelectTourList}>관광지 검색</button>
-        <button onClick={update}>update</button>
+      </Tripbox>
+      <Tripbox>
+        <TripSelect className={slidemode ? "on" : "off"}>
+          <div>
+            {slidemode ? (
+              <div style={{ textAlign: "center" }}>장소를 선택해주세요</div>
+            ) : (
+              <div
+                style={{
+                  fontWeight: 900,
+                  textAlign: "center",
+                  fontSize: "32px",
+                }}
+              >
+                0
+              </div>
+            )}
+          </div>
+        </TripSelect>
+        <ModeController onClick={handleSlidemode}>
+          {slidemode ? (
+            <img
+              src="/img/Left.svg"
+              style={{ width: "30px", height: "30px" }}
+            />
+          ) : (
+            <img
+              src="/img/Right.svg"
+              style={{ width: "30px", height: "30px" }}
+            />
+          )}
+        </ModeController>
       </Tripbox>
     </PlaceWrapper>
   );
