1385661a60f8b384baacadd580add074786908b0
sunghyun1014 <sunghyun1014@naver.com>
2024-01-06 16:45:36 +0900

    MainContainer 에 framer motion 적용

--- src/main/reactfront/src/routers/Main.js
+++ src/main/reactfront/src/routers/Main.js
@@ -1,13 +1,34 @@
+import styled from "styled-components";
 import MainSection from "../components/MainSection";
 import Navi from "../components/Navi";
-import "../css/Main.css";
 
+import { motion } from "framer-motion";
+
+const MainContainer = styled(motion.div)`
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+`;
 function Main() {
   return (
-    <div className="main_container">
+    <MainContainer
+      initial={{ opacity: 0, y: "30%" }}
+      animate={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          type: "spring", // 스프링 효과 사용
+          damping: 10, // 감쇠 설정
+          stiffness: 30, // 강성 설정
+          duration: 2,
+        },
+      }}
+      exit={{ opacity: 0, y: "-100%" }}
+    >
       <Navi />
       <MainSection />
-    </div>
+    </MainContainer>
   );
 }
 
