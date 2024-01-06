7c1da8e2d81f45f63fd63ec658f2e6b300f3e142
sunghyun1014 <sunghyun1014@naver.com>
2024-01-06 16:42:57 +0900

    Link 컴포넌트  경로 변경

--- src/main/reactfront/src/components/Header.js
+++ src/main/reactfront/src/components/Header.js
@@ -2,19 +2,17 @@ import { Link } from "react-router-dom";
 import "../css/header.css";
 import img from "../img/CHlogo.png";
 
-function Header() {
+export default function Header() {
   return (
     <div className="header_container one">
       <div className="header_items">
         <img src={img} alt="" className="icon" />
       </div>
       <div className="header_items two">
-        <Link to="/info" className="Link ">
+        <Link to="/how" className="Link">
           이용방법
         </Link>
       </div>
     </div>
   );
 }
-
-export default Header;
