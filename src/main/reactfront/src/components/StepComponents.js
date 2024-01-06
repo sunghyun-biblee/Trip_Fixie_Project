83b2e22e583934a6508f2dc63291296b14c435e4
sunghyun1014 <sunghyun1014@naver.com>
2024-01-06 16:43:35 +0900

    css 수정

--- src/main/reactfront/src/components/StepComponents.js
+++ src/main/reactfront/src/components/StepComponents.js
@@ -5,6 +5,8 @@ export const StepContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  background-color: white;
 `;
 export const Stepbox = styled.div`
   display: flex;
@@ -21,6 +23,8 @@ export const StepUl = styled.ul`
 `;
 export const StepLi = styled.li`
   color: gray;
+  scale: 0.8;
+  transition: scale 0.5s;
   width: 11rem;
   text-align: center;
   padding: 3.2rem 0;
