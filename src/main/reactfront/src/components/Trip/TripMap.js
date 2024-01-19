681462ff73fca67b4f56b2286282b6c2c3755177
sunghyun1014 <sunghyun1014@naver.com>
2024-01-19 15:34:49 +0900

    위시리스트에 관광지나 행사를 추가하면 지도에 마커가 찍히고 , 위시리스트에 추가된 관광지나 행사를 클릭하면 지도가 해당 위치로 포커싱되며, 마커를 클릭시 해당 장소의 상세정보를 출력함 관광지나 행사리스트에서 상세정보를 클릭시 맵위에 상세정보를 출력

--- src/main/reactfront/src/components/Trip/TripMap.js
+++ src/main/reactfront/src/components/Trip/TripMap.js
@@ -1,13 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./css/TripMap.css";
 import axios from "axios";
+import styled from "styled-components";
+
+const ShowPlaceInfo = styled.div`
+  background-color: black;
+  color: white;
+  position: absolute;
+  top: 40%;
+  left: 10%;
+`;
 
 function TripMap({
   selectedAreaName,
   mygeolocation,
   setMygeolocation,
   saveTourList,
+  detailData,
+  setDetailData,
 }) {
+  const [marker, setMarker] = useState([]);
+
+  const [selectedPlaceInfo, setSelectedPlaceInfo] = useState(null);
   useEffect(() => {
     const mapcontainer = document.getElementById("map");
     const map = new window.naver.maps.Map(mapcontainer, {
@@ -18,16 +32,19 @@ function TripMap({
       zoom: 14,
     });
 
-    saveTourList.map((list) => {
-      const marker = [
-        new window.naver.maps.Marker({
-          position: new window.naver.maps.LatLng(
-            list.clatitude,
-            list.clongitude
-          ),
-          map: map,
-        }),
-      ];
+    saveTourList.map((list, index) => {
+      const marker = new window.naver.maps.Marker({
+        position: new window.naver.maps.LatLng(list.clatitude, list.clongitude),
+        map: map,
+      });
+      marker.addListener("click", () => {
+        showInfo(saveTourList[index]);
+      });
+      setMarker((prev) => [...prev, marker]);
+      // window.naver.maps.Event.addListener(marker[index], "click", () => {
+      //   showInfo(index);
+      //   // console.log(saveTourList[index]);
+      // });
     });
   }, [mygeolocation, saveTourList]);
 
@@ -49,13 +66,61 @@ function TripMap({
     }
   }, [selectedAreaName]);
 
+  const showInfo = (data) => {
+    // setSelectedPlaceInfo((prev) => {
+    //   console.log(prev);
+
+    //   if (prev !== null) {
+    //     setSelectedPlaceInfo(null);
+    //   } else {
+    //     setSelectedPlaceInfo(data);
+    //   }
+    // });
+
+    if (data) {
+      const savelistDataId = data.contentid;
+      const url = `http://apis.data.go.kr/B551011/KorService1/detailCommon1?MobileOS=ETC&MobileApp=APPTest&serviceKey=cHlc2k2XcgjG10dgBDyoxMaS6KxKLHiHN4xtTP6q86EBe%2BUO09zOLEg6ZTpX9TWrdJPSJcFQYCZ%2B6fqhkD2ZVA%3D%3D&contentId=${savelistDataId}&overviewYN=Y&_type=json`;
+      try {
+        axios.get(url).then((response) => {
+          const overview = response.data.response.body.items.item[0].overview;
+          setSelectedPlaceInfo({ ...data, overview });
+        });
+      } catch (error) {
+      } finally {
+        setMygeolocation({ lat: data.clatitude, long: data.clongitude });
+      }
+    }
+  };
+  useEffect(() => {
+    showInfo(detailData);
+    console.log(detailData);
+  }, [detailData]);
+  console.log(selectedPlaceInfo);
   return (
-    <div id="map" className="Map">
-      {/* {UserPosition.latitude === ""
-        ? "loading"
-        : `${Math.floor(UserPosition.latitude)}+${Math.floor(
-            UserPosition.longitude
-          )}`} */}
+    <div style={{ position: "relative", width: "100%" }} className="TLQkf">
+      <div id="map" className="Map">
+        {/* {UserPosition.latitude === ""
+          ? "loading"
+          : `${Math.floor(UserPosition.latitude)}+${Math.floor(
+              UserPosition.longitude
+            )}`} */}
+      </div>
+      {selectedPlaceInfo ? (
+        <ShowPlaceInfo>
+          <p>{selectedPlaceInfo.ctitle}</p>
+          <p>{selectedPlaceInfo.caddr1}</p>
+          <p>{selectedPlaceInfo.caddr2}</p>
+          <p>{selectedPlaceInfo.contenttypeid}</p>
+          <p>{selectedPlaceInfo.overview}</p>
+          <div
+            style={{ width: "30px", height: "30px", backgroundColor: "red" }}
+            onClick={() => {
+              setDetailData(null);
+              setSelectedPlaceInfo(null);
+            }}
+          ></div>
+        </ShowPlaceInfo>
+      ) : null}
     </div>
   );
 }
